930333c8b60e2169e5f2317bc03968dbd179b0a0
tranki1 <kimhieutran100@gmail.com>
2018-06-16 15:48:39 +0300

    Add notice when burger ingredient is empty

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,12 +7,20 @@ const burger = (props) => {
     //tranform object to array
     //keys(): extracts the keys of a given ojects and turn that in to an array
 
-    const transformedIngredients=Object.keys(props.ingredients)
+    let transformedIngredients=Object.keys(props.ingredients)
         .map(igKey =>{
         return [...Array(props.ingredients[igKey])].map((_,i)=>{
             return <BurgerIngredient key={igKey + i} type={igKey}/>;
         })
-    });
+    })
+    .reduce((arr,el)=>{
+        return arr.concat(el)
+    },[]);
+    //reduce array tocheck if array empty
+    if(transformedIngredients.length===0){
+        transformedIngredients=<p>Please start adding ingredients!</p>
+    }
+    console.log(transformedIngredients);
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top'/>
