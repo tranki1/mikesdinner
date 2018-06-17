e1eb7f72d765fa59883abcebf6474142067a0aab
tranki1 <kimhieutran100@gmail.com>
2018-06-17 22:44:46 +0300

    Add reusable NavItem

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,13 +3,14 @@ import Aux from "../../hoc/Aux";
 import Toolbar from '../UI/Navigation/Toolbar/Toolbar';
 import classes from "./Layout.css";
 import Logo from "../UI/Navigation/Logo/Logo";
+import NavigationItems from "../UI/Navigation/NavigationItems/NavigationItems";
 const layout = props => {
   return (
     <Aux>
       <Toolbar>
         <div>Menu</div>
         <Logo/>
-        <div>Nav</div>
+        <NavigationItems></NavigationItems>
       </Toolbar>
       <main className={classes.Content}>{props.children}</main>
     </Aux>
