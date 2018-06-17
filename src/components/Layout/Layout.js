14e972dd9939af3337ebc443e0768177149ab66e
tranki1 <kimhieutran100@gmail.com>
2018-06-17 22:17:50 +0300

    Adding Logo dynamically

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,13 @@ import React from "react";
 import Aux from "../../hoc/Aux";
 import Toolbar from '../UI/Navigation/Toolbar/Toolbar';
 import classes from "./Layout.css";
+import Logo from "../UI/Navigation/Logo/Logo";
 const layout = props => {
   return (
     <Aux>
       <Toolbar>
         <div>Menu</div>
-        <div>Logo</div>
+        <Logo/>
         <div>Nav</div>
       </Toolbar>
       <main className={classes.Content}>{props.children}</main>
